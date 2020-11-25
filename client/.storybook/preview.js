882995c5f222fcced06810b64c3cccbba0517a12
AlexisRoe <A.Roehrling@gmx.de>
2020-11-25 09:42:00 +0100

    :art: Add Globalstyle and connect it with storybook

--- client/.storybook/preview.js
+++ client/.storybook/preview.js
@@ -1,3 +1,16 @@
+import React from "react";
+import Globalstyle from "../src/Globalstyle";
+
+export const decorators = [
+  (Story) => {
+    <>
+      <Globalstyle />
+      <Story />
+    </>;
+  },
+];
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
+  layout: "fullscreen",
 };
