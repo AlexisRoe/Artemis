e9abe4e9960a1bc53f8b772bb17adb62a7a80390
AlexisRoe <A.Roehrling@gmx.de>
2020-12-01 11:04:36 +0100

    Add Pages to routes

---
+++ client/src/Pages/Login/index.js
@@ -0,0 +1,22 @@
+import { LoginButton } from "../../components/Login/LoginButton";
+import { LoginInput } from "../../components/Login/LoginInput";
+import { Logo } from ".../../components/Login/Logo";
+import { LoginForm } from "../../components/Login/LoginForm";
+import { LoginContainer } from "../../components/Login/LoginContainer";
+import { ProgressNotification } from "../../components/Menu/ProgressNotification";
+
+function Login() {
+  return (
+    <LoginContainer>
+      <Logo />
+      <LoginForm>
+        <ProgressNotification state="error" />
+        <LoginInput title="Mitarbeiter-ID" />
+        <LoginInput title="Password" />
+        <LoginButton type="submit">Login</LoginButton>
+      </LoginForm>
+    </LoginContainer>
+  );
+}
+
+export default Login;
