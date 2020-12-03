80636f3671833f0bc326eadc7a6994fdb6060b48
AlexisRoe <A.Roehrling@gmx.de>
2020-12-03 11:13:15 +0100

    ✨ Add unix timestamp features

--- client/src/utils/dates/Date.js
+++ client/src/utils/dates/Date.js
@@ -1,8 +1,18 @@
-export function isDate() {
-  const today = new Date();
-  const date = `${today.getDate()}.${
-    today.getMonth() + 1
-  }.${today.getFullYear()}`;
+// date-format: DD.MM.YYYY
+export const isDate = (today = Date.now()) =>
+  `${today.getDate().slice(-2)}.${(today.getMonth() + 1).slice(
+    -2
+  )}.${today.getFullYear()}`;
 
-  return date;
-}
+// time-format: MM:HH
+export const isTime = (time = Date.now()) =>
+  `${time.getMinutes().slice(-2)}.${time.getHours().slice(-2)}`;
+
+// Unix Timestamps
+export const isUnixTime = () => Math.floor(Date.now() / 1000);
+
+export const convertDatefromUnixTime = (unixTimestamp) =>
+  isDate(new Date(unixTimestamp * 1000));
+
+export const convertTimefromUnixTime = (unixTimestamp) =>
+  isTime(new Date(unixTimestamp * 1000));
