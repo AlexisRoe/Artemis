1547adf087bb413c77130288b7b37b3236651e0f
AlexisRoe <A.Roehrling@gmx.de>
2020-12-09 16:00:25 +0100

    ✨ Add JWT and Cookie authorization to app

--- server.js
+++ server.js
@@ -1,17 +1,19 @@
 require("dotenv").config();
 const express = require("express");
 const path = require("path");
+const cookieParser = require("cookie-parser");
+
 const { connect } = require("./lib/api/database");
+
 const event = require("./lib/routes/eventID");
 const overview = require("./lib/routes/overviewDay");
 const login = require("./lib/routes/login");
 
 const app = express();
 const port = process.env.PORT || 6000;
+
 app.use(express.json());
-
-// TODO: construct middleware to protect routes
-
+app.use(cookieParser());
 app.use("/api/event", event);
 app.use("/api/date", overview);
 app.use("/api/user", login);
