9c07198135bf761c3d2076b09b3c261ac1ef4e5b
AlexisRoe <A.Roehrling@gmx.de>
2020-12-12 19:03:05 +0100

    🐛 fix background-image

--- client/src/components/helper/missingData.js
+++ client/src/components/helper/missingData.js
@@ -1,11 +1,15 @@
 import styled from "styled-components";
-import src from "../../assets/logo/logo-artemis.png";
+import logo from "../../assets/logo/logo-bg.svg";
 
 const Div = styled.div`
   width: 100%;
-  min-height: 350px;
+  min-height: 250px;
   display: grid;
   place-items: center;
+  background-image: url(${logo});
+  background-repeat: no-repeat;
+  background-size: 25%;
+  background-position: center center;
 
   img {
     z-index: -1;
@@ -15,6 +19,7 @@ const Div = styled.div`
 
   h4 {
     width: 200px;
+    text-align: center;
   }
 `;
 
@@ -22,10 +27,9 @@ export default function MissingData() {
   return (
     <Div>
       <h4>
-        nothing to found to do <br />
+        nothing found to do <br />
         in the next 6 hours
       </h4>
-      <img href={src} alt="" />
     </Div>
   );
 }
